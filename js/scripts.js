044bba8537cec15fc58422134f69bc54ac321510
nachi <nachiaerotech@gmail.com>
2025-01-05 11:59:49 -0500

    added google analytics, updated copyright year

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2022 Start Bootstrap
+* Copyright 2013-2025 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
